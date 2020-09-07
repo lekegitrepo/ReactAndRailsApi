78f08b1a38ac93746fc68521fb50252a5dc610a8
lekegitrepo <lekedevrepo@gmail.com>
2020-09-07 08:13:26 -0500

    create registration form

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class Registration extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('form submitted!')
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>Registration</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type='email' name='email' placeholder='Email' value={this.state.email} onChange={this.handleChange} required/>
+          <input type='password' name='password' placeholder='Password' value={this.state.password} onChange={this.handleChange} required/>
+          <input type='password' name='password_confirmation' placeholder='Password Confirmation' value={this.state.password_confirmation} onChange={this.handleChange} required/>
+        
+          <button type='submit'> Register </button>
+        </form>
       </div>
     );
   }
