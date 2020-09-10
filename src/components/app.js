e75aa1c3d00e0b54955fdd54f2180d3507f5f4fb
lekegitrepo <lekedevrepo@gmail.com>
2020-09-10 02:42:19 -0500

    add render props to the home route

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,27 @@ import Home from './Home';
 import Dashboard from './Dashboard';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGIN",
+      user: {}
+    }
+  }
+
   render() {
     return (
       <div className='app'>
         <BrowserRouter>
           <Switch>
-            <Route exact path={'/'} component={Home} />
+            <Route
+              exact
+              path={'/'}
+              render={props => (
+                <Home {...props} loggedInStatus={this.state.loggedInStatus} />
+              )}
+            />
             <Route exact path={'/dashboard'} component={Dashboard} />
           </Switch>
         </BrowserRouter>
